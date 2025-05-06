a137e06dfc06daf80da0ad20182df3ab501e6721
nckfranco <143292684+NicolasFranco022@users.noreply.github.com>
2025-05-06 13:28:05 -0300

    Tinha esquecido de adicionar
    .

---
+++ atividades/Atividade6-BootStrapPT2/assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+function showMenu() {
+    document.querySelector('#menu').style.marginLeft = 0 ;
+}
+
+function hideMenu() {
+    document.querySelector('#menu').style.marginLeft = '-250px';
+}
